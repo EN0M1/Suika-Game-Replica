59c69716a772ad92fee3101a9787f2434e8a5896
EN0M1 <eli.mizerski@gmail.com>
2025-03-05 12:57:39 -0600

    Fixes
    Title

--- Assets/Scipts/SpawnBehavior.cs
+++ Assets/Scipts/SpawnBehavior.cs
@@ -14,7 +14,7 @@ public class SpawnBehavior : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        cooldown = 0;
+    
     }
 
     // Update is called once per frame
